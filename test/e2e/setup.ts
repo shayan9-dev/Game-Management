7103f797cd3a5acac73b83523599ada914c276c8
shayan9-dev <shayanahmed8862@gmail.com>
2025-08-02 22:57:12 +0500

    describe

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -10,5 +10,29 @@ export async function createTestApp(): Promise<INestApplication> {
 
     const app = moduleRef.createNestApplication();
     await app.init();  
-    return app  
+    return app
 };
+
+// describe('Login Function', () => {
+
+//   it('should return success for correct credentials', () => {
+//     const result = login("admin", "1234");
+//     expect(result).toBe("Login successful");
+//   });
+
+//   it('should return failure for incorrect username', () => {
+//     const result = login("user", "1234");
+//     expect(result).toBe("Login failed");
+//   });
+
+//   it('should return failure for incorrect password', () => {
+//     const result = login("admin", "wrong");
+//     expect(result).toBe("Login failed");
+//   });
+
+//   it('should return failure for both wrong username and password', () => {
+//     const result = login("user", "wrong");
+//     expect(result).toBe("Login failed");
+//   });
+
+// });
