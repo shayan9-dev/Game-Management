7018fc9951f84ea48765b0560e898fe52626356d
shayan9-dev <shayanahmed8862@gmail.com>
2025-08-03 16:26:32 +0500

    fixing

--- test/e2e/setup.ts
+++ test/e2e/setup.ts
@@ -1,15 +1,13 @@
-import { INestApplication } from "@nestjs/common";
-import { Test } from "@nestjs/testing";
-import { promises } from "dns";
-import { AppModule } from "src/app.module";
-
+import { INestApplication } from '@nestjs/common';
+import { Test } from '@nestjs/testing';
+import { AppModule } from 'src/app.module';
 
 export async function createTestApp(): Promise<INestApplication> {
-    const moduleRef = await Test.createTestingModule({
-        imports: [AppModule],
-    }).compile();
+  const moduleRef = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
 
-    const app = moduleRef.createNestApplication();
-    await app.init();  
-    return app
-};
+  const app = moduleRef.createNestApplication();
+  await app.init();
+  return app;
+}
